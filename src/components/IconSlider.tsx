00b649d360ac0efba2a4ecf6852b5b18e3f97241
coderbuggy <coderbuggy@gmailcom>
2024-07-29 15:13:05 +0300

    projects

--- src/components/IconSlider.tsx
+++ src/components/IconSlider.tsx
@@ -5,108 +5,110 @@ const IconSlider = () => {
     <div className="p-10 flex flex-wrap justify-center gap-4">
       <Image
         src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="html"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="css"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDML5CFq70Y9FJ52YnyCjfdyUA3g9B6is_jA&s"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="tailwind"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/styled-components/styled-components.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="styled-components"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="js"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://cdn-icons-png.flaticon.com/128/5968/5968381.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="ts"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://cdn-icons-png.flaticon.com/128/875/875209.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="react"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://cdn-icons-png.flaticon.com/128/919/919825.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="nodejs"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="expressjs"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://cdn.worldvectorlogo.com/logos/mongodb-icon-2.svg"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="mongodb"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://www.datocms-assets.com/98835/1684410508-image-7.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="nextjs"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSf6vvGX2uMfpt06Tb5pLRRWgC6W4V7Sgz2A&s"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="sapui5"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
       <Image
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/SAP_2011_logo.svg/2560px-SAP_2011_logo.svg.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="sap"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
+
       <Image
         src="https://devblogs.microsoft.com/odata/wp-content/uploads/sites/23/2019/02/ODataLogo-250.png"
-        width={50}
-        height={50}
+        width={60}
+        height={60}
         alt="odata"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
+
       <Image
-        src="https://cdn-icons-png.flaticon.com/128/136/136526.png"
-        width={50}
-        height={50}
+        src="https://cdn-icons-png.flaticon.com/128/9496/9496555.png"
+        width={60}
+        height={60}
         alt="xml"
-        className="flex-shrink-0"
+        className="flex-shrink-0 rounded-full"
       />
     </div>
   );
