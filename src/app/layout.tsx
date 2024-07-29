56b10d7f7761dd2d219809b3562c80ccbdbf6bf1
coderbuggy <coderbuggy@gmailcom>
2024-07-29 23:37:55 +0300

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Header />
-        {children}
+        <header>
+          <Header />
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
